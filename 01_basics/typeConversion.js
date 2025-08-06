dfdc686e8811a8a881430f096a37feb5eaac92c7
Kumar Dandvekar <94390889+kumardandvekar@users.noreply.github.com>
2025-08-06 18:41:22 +0000

    addon

--- 01_basics/typeConversion.js
+++ 01_basics/typeConversion.js
@@ -13,3 +13,12 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(isLoggedIn);
+
+// Operations
+
+console.log("1" + 2 + 2);
+// 122 string first all string
+console.log(1 + 2 + "2");
+// 32 string last then conversion first
+
+console.log(true);   
