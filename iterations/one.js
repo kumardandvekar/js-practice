2578c462809544e4aa826946a642173b8697d1f4
Kumar Dandvekar <94390889+kumardandvekar@users.noreply.github.com>
2025-08-29 11:03:55 +0000

    change

---
+++ iterations/one.js
@@ -0,0 +1,12 @@
+//for 
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if(element ==5)
+//     {
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(element);
+// }
+
